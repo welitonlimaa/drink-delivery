c9b4b7c8f766f724526e6af59b0f0320e81b32ac
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-05-03 18:44:52 -0300

    Chore: configurando docker compose e config.js

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const environment = process.env.NODE_ENV || 'test';
 const suffix = {
   prod: '',
@@ -10,8 +11,9 @@ const suffix = {
 const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database:
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  database: `${process.env.MYSQL_DB_NAME || 'delivery-app'}${
+    suffix[environment] || suffix.test
+  }`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
