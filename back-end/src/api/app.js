54e5d1b54f33bade5a2c75cefced6739ab3c0e5e
welitonlimaa <welitonlima159@gmail.com>
2023-05-03 17:13:15 -0300

    fix: correction

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,14 +1,11 @@
-const { userRoutes } = require('../routers')
-
 const express = require('express');
+const { userRoutes } = require('../routers');
 
 const app = express();
 app.use(express.json());
 
-app.use('/login', userRoutes)
+app.use('/login', userRoutes);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
-
-
 
 module.exports = app;
