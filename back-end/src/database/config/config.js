c1d8611a0198b4564bd19aed857e86bd1ecfeb9d
welitonlimaa <welitonlima159@gmail.com>
2023-05-03 17:10:47 -0300

    chore: database config connection

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -9,14 +9,14 @@ const suffix = {
   development: '-dev',
   test: '-test',
 };
-// process.env.HOSTNAME
+
 const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3002',
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || '3306',
   database:
     `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '123456',
+  password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
