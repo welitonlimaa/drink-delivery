8f6dc4b8a5b24a0476a5b730c3d63ca345783e87
welitonlimaa <welitonlima159@gmail.com>
2023-05-04 19:47:18 -0300

    chore: .env back

--- front-end/src/Routers/Router.js
+++ front-end/src/Routers/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Register from '../Pages/Register';
 import Login from '../Pages/Login';
 import Products from '../Pages/Products';
@@ -7,8 +7,10 @@ import Products from '../Pages/Products';
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/login" component={ Login } />
+      <Route path="/login" component={ Login } />
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
       <Route path="/register" component={ Register } />
       <Route path="/customer/products" component={ Products } />
     </Switch>
