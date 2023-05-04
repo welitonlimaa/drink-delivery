239ae2793aa6f44f0b3a5c25971078e06ea1f34b
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-05-04 09:23:13 -0300

    Fix: correções nas páginas de login e register para respeitar o eslint

--- front-end/src/Pages/Login.js
+++ front-end/src/Pages/Login.js
@@ -1,22 +1,38 @@
 import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import AppContext from '../context/AppContext';
+import logo from '../images/logo.png';
 
 function Login() {
-  const { email, setEmail } = useContext(AppContext);
+  const { email, setEmail, password, setPassword } = useContext(AppContext);
   const history = useHistory();
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    switch (name) {
+    case 'email':
+      setEmail(value);
+      break;
+    case 'password':
+      setPassword(value);
+      break;
+    default:
+      break;
+    }
+  };
 
   const handleClick = () => {
     history.push('/register');
   };
 
   return (
-    <div>
-      <h1 className="font-bold text-2xl">Welcome Back :)</h1>
+    <div className="flex justify-center">
       <form
-        className="flex flex-col bg-white rounded shadow-lg p-12 mt-12"
+        className="flex flex-col justify-center bg-white rounded shadow-lg p-12"
         action=""
       >
+        <img className="w-200 mr-2" src={ logo } alt="logo" />
         <label
           className="font-semibold text-xs"
           htmlFor="email-input"
@@ -27,6 +43,9 @@ function Login() {
             bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"
             type="text"
             id="email-input"
+            name="email"
+            value={ email }
+            onChange={ (e) => handleChange(e) }
           />
         </label>
         <label
@@ -38,24 +57,37 @@ function Login() {
             className="flex items-center h-12 px-4 w-64
             bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"
             type="password"
+            name="password"
             id="password-input"
+            value={ password }
+            onChange={ (e) => handleChange(e) }
           />
         </label>
         <button
-          className="flex items-center justify-center h-12 px-6 w-64
-          bg-blue-600 mt-8 rounded font-semibold text-sm
-          text-blue-100 hover:bg-blue-700"
+          className="flex items-center
+          justify-center
+          h-12
+          px-6
+          w-64
+          bg-blue-600
+          mt-8
+          rounded
+          font-semibold
+          text-sm
+          text-blue-100
+          hover:bg-blue-700"
           type="button"
+          onClick={ handleClick }
         >
           Login
         </button>
         <div className="flex mt-6 justify-center text-xs">
-          <a
+          <Link
             className="text-blue-400 hover:text-blue-500"
-            href=""
+            to="/register"
           >
             Sign Up
-          </a>
+          </Link>
         </div>
       </form>
     </div>
--- front-end/src/context/AppProvider.js
+++ front-end/src/context/AppProvider.js
@@ -2,29 +2,33 @@ import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
-function AppProvider( { children } ) {
+function AppProvider({ children }) {
   const [isLoading, setIsLoading] = useState(true);
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const values = useMemo(() => ({
     email,
     setEmail,
+    password,
+    setPassword,
     isLoading,
     setIsLoading,
   }), [
     isLoading,
     email,
-  ])
+    password,
+  ]);
 
   return (
     <AppContext.Provider value={ values }>
       { children }
     </AppContext.Provider>
-  )
+  );
 }
 
 AppProvider.propTypes = {
-  children: PropTypes.shape({}),
+  children: PropTypes.shape({}).isRequired,
 };
 
 export default AppProvider;
