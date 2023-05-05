693a3f33f37a3541963f655b4f9baf2ad5def521
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-05-05 11:00:45 -0300

    Fix: validation of the password length in the back-end

--- back-end/src/services/validations/schemas.js
+++ back-end/src/services/validations/schemas.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const dataLoginSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
+  password: Joi.string().min(6).required(),
 });
 
 const newUserSchema = Joi.object({
