10652ca3eb4c24d51a26792bf6e001e6d6b898e5
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-05-05 17:24:01 -0300

    feat: created routes of the header links

--- front-end/src/Routers/Router.js
+++ front-end/src/Routers/Router.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Register from '../Pages/Register';
 import Login from '../Pages/Login';
-import Products from '../Pages/Products';
-import Checkout from '../Pages/Checkout';
+import customerProducts from '../Pages/customerProducts';
+import customerCheckout from '../Pages/customerCheckout';
+import sellerOrders from '../Pages/sellerOrders';
+import adminManage from '../Pages/adminManage';
+import customerOrders from '../Pages/customerOrders';
 
 function Routes() {
   return (
@@ -13,8 +16,11 @@ function Routes() {
         <Redirect to="/login" />
       </Route>
       <Route path="/register" component={ Register } />
-      <Route path="/customer/checkout" component={ Checkout } />
-      <Route path="/customer/products" component={ Products } />
+      <Route path="/customer/checkout" component={ customerCheckout } />
+      <Route path="/customer/products" component={ customerProducts } />
+      <Route path="/customer/orders" component={ customerOrders } />
+      <Route path="/seller/orders" component={ sellerOrders } />
+      <Route path="/admin/manage" component={ adminManage } />
     </Switch>
   );
 }
