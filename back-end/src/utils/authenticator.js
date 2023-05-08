e2729d1bcdc1570d5c080969148ea7f2a53e1e15
welitonlimaa <welitonlima159@gmail.com>
2023-05-08 17:24:13 -0300

    refactor: secret key jwt token create

--- back-end/src/utils/authenticator.js
+++ back-end/src/utils/authenticator.js
@@ -1,6 +1,10 @@
 const jwt = require('jsonwebtoken');
+const path = require('path');
+const fs = require('fs');
 
-const secret = process.env.JWT_SECRET || 'naruto';
+const JWT_SECRET = path.resolve(__dirname, '../../jwt.evaluation.key');
+
+const secret = fs.readFileSync(JWT_SECRET, 'utf8');
 
 const JWT_CONFIG = {
   algorithm: 'HS256',
