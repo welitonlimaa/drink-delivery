78acab241948e089dcd263a2f599200a5722accc
welitonlimaa <welitonlima159@gmail.com>
2023-05-10 14:54:42 -0300

    feat: request order data and create loading component

--- front-end/src/Pages/CustomerOrderId.js
+++ front-end/src/Pages/CustomerOrderId.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { requestData, setToken } from '../services/requests';
+import Loading from '../components/Loading';
 
 function CustomerOrderId() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [orderData, setOrderData] = useState({});
+  const params = useParams();
+
+  const requestOrderData = async (id) => {
+    const { token } = JSON.parse(localStorage.getItem('user'));
+    setToken(token);
+
+    const data = await requestData(`/sales/${id}`);
+    setOrderData(data);
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    requestOrderData(params.id);
+  }, []);
+
+  if (isLoading) return <Loading />;
+
   return (
-    <div>CustomerOrderId</div>
+    <div>
+      <span>{ orderData.id }</span>
+      P. Vend:
+      <span>a</span>
+      <span>a</span>
+      <span>a</span>
+      <button
+        type="button"
+      >
+        MARCAR COMO ENTREGUE
+      </button>
+    </div>
   );
 }
 
