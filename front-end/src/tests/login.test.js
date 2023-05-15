82a794c3e183109d91a0cf16140d18cb552b63b5
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-05-15 14:26:48 -0300

    test: begging the sucessfull login test

--- front-end/src/tests/login.test.js
+++ front-end/src/tests/login.test.js
@@ -7,7 +7,7 @@ import AppProvider from '../context/AppProvider';
 import renderWithRouter from '../utils/renderWithRouter';
 
 describe('Login page', () => {
-  test('login form is rende', () => {
+  test('login form is rende', async () => {
     const { history } = renderWithRouter(
       <AppProvider>
         <App />
@@ -17,16 +17,34 @@ describe('Login page', () => {
     const inputEmail = screen.getByTestId('common_login__input-email');
     const inputPassword = screen.getByTestId('common_login__input-password');
     const buttonLogin = screen.getByTestId('common_login__button-login');
+    const invalidAlert = screen.getByTestId('common_login__element-invalid-email');
 
     expect(history.location.pathname).toBe('/login');
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
     expect(buttonLogin).toBeInTheDocument();
+    expect(invalidAlert).not.toBeInTheDocument();
 
     expect(buttonLogin.disabled).toBe(true);
 
     userEvent.type(inputEmail, loginCustomerData.email);
     userEvent.type(inputPassword, loginCustomerData.password);
     expect(buttonLogin.disabled).toBe(false);
+  });
+
+  test('realiza login com sucesso', () => {
+    renderWithRouter(
+      <AppProvider>
+        <App />
+      </AppProvider>,
+    );
+
+    const inputEmail = screen.getByTestId('common_login__input-email');
+    const inputPassword = screen.getByTestId('common_login__input-password');
+    const buttonLogin = screen.getByTestId('common_login__button-login');
+
+    expect(inputEmail.value).toBe('');
+    userEvent.type(inputEmail, loginCustomerData.email);
+    expect(inputEmail.value).toBe(loginCustomerData.email);
   });
 });
