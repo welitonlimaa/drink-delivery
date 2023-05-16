4435406785ad50ba7980456b9eee82c7393f099a
welitonlimaa <welitonlima159@gmail.com>
2023-05-16 19:02:49 -0300

    style: default theme color

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
       flamingo: {
         50: '#fef3f2',
         100: '#fee5e2',
