ed1cb8489dbf2b17eb4f1fe6889bff962c18f384
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-05-18 10:17:15 -0300

    fix: lint of OrderCard

--- front-end/src/components/OrderCard.js
+++ front-end/src/components/OrderCard.js
@@ -16,63 +16,10 @@ function OrderCard({ order }) {
     deliveryNumber,
   } = order;
 
-  const colorStatus = {
-    Entregue: 'green',
-    Pendente: 'red',
-    Preparando: 'yellow',
-    'Em Trânsito': 'orange',
-  };
-
   const digits = 4;
 
   return (
     <div>
-      {/* <Link
-        to={ `/${role}/orders/${id}` }
-        className="flex bg-slate-50 m-1 w-72 text-center justify-evenly p-2 shadow-md
-        shadow-slate-600 hover:brightness-125 flex-wrap"
-      >
-        <div className="bg-white p-1">
-          <p>Pedido</p>
-          <h2 data-testid={ `${role}_orders__element-order-id-${id}` }>{ id }</h2>
-        </div>
-        <div
-          className={
-            `flex align-middle text-center capitalize bg-${colorStatus[status]}-400 p-2`
-          }
-        >
-          <p
-            data-testid={ `${role}_orders__element-delivery-status-${id}` }
-          >
-            { status }
-          </p>
-        </div>
-        <div>
-          <p
-            className="bg-white p-1"
-            data-testid={ `${role}_orders__element-order-date-${id}` }
-          >
-            { convertDateFormat(saleDate, digits) }
-          </p>
-          <p
-            className="bg-white p-1"
-            data-testid={ `${role}_orders__element-card-price-${id}` }
-          >
-            { totalPrice.replace(/\./, ',') }
-          </p>
-        </div>
-        {
-          role !== 'seller' ? null : (
-            <div className="text-xs w-full text-right">
-              <p data-testid={ `seller_orders__element-card-address-${id}` }>
-                {
-                  `${deliveryAddress}, ${deliveryNumber}`
-                }
-              </p>
-            </div>
-          )
-        }
-      </Link> */}
       <Link
         to={ `/${role}/orders/${id}` }
         className="flex justify-evenly flex-col items-center m-1 w-72 text-center
